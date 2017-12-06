8b537bdf24e14d988a58d46be64751a194cbf550
linyuzai <120718461@qq.com>
2017-12-06 16:42:28 +0800

    modify BaseRx by myself

--- mvp-rx2/src/main/java/com/linyuzai/mvp/rx2/base/BaseRx2Activity.kt
+++ mvp-rx2/src/main/java/com/linyuzai/mvp/rx2/base/BaseRx2Activity.kt
@@ -1,39 +1,68 @@
 package com.linyuzai.mvp.rx2.base
 
 import android.os.Bundle
-import com.linyuzai.mvp.base.UIComponent
-import com.linyuzai.mvp.theme.MicroTheme
-import com.linyuzai.mvp.theme.MicroThemeChangeable
-import com.linyuzai.mvp.theme.MicroThemeManager
-import com.trello.rxlifecycle2.components.RxActivity
+import android.support.annotation.CallSuper
+import android.support.annotation.CheckResult
+import com.linyuzai.mvp.base.BaseActivity
+import com.trello.rxlifecycle2.LifecycleProvider
+import com.trello.rxlifecycle2.LifecycleTransformer
+import com.trello.rxlifecycle2.RxLifecycle
+import com.trello.rxlifecycle2.android.ActivityEvent
+import com.trello.rxlifecycle2.android.RxLifecycleAndroid
+import io.reactivex.Observable
+import io.reactivex.subjects.BehaviorSubject
 
 /**
  * Created by linyuzai on 2017/10/26.
  * The base activity to extends
  * @author linyuzai
  */
-abstract class BaseRx2Activity : RxActivity(), UIComponent, MicroThemeChangeable {
+abstract class BaseRx2Activity : BaseActivity(), LifecycleProvider<ActivityEvent> {
+    private val lifecycleSubject = BehaviorSubject.create<ActivityEvent>()
+    @CheckResult
+    override fun lifecycle(): Observable<ActivityEvent> = this.lifecycleSubject.hide()
+
+    @CheckResult
+    override fun <T> bindUntilEvent(event: ActivityEvent): LifecycleTransformer<T> =
+            RxLifecycle.bindUntilEvent(this.lifecycleSubject, event)
+
+    @CheckResult
+    override fun <T> bindToLifecycle(): LifecycleTransformer<T> =
+            RxLifecycleAndroid.bindActivity(this.lifecycleSubject)
+
+    @CallSuper
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        layout()
-        val theme = MicroThemeManager.getCurrentTheme()
-        if (theme != null)
-            onThemeChange(theme)
-        view(savedInstanceState)
-        MicroThemeManager.register(this)
+        this.lifecycleSubject.onNext(ActivityEvent.CREATE)
     }
 
-    /**
-     * layout the ui
-     */
-    abstract fun layout()
+    @CallSuper
+    override fun onStart() {
+        super.onStart()
+        this.lifecycleSubject.onNext(ActivityEvent.START)
+    }
 
-    override fun view(savedInstanceState: Bundle?) {}
+    @CallSuper
+    override fun onResume() {
+        super.onResume()
+        this.lifecycleSubject.onNext(ActivityEvent.RESUME)
+    }
 
-    override fun onThemeChange(theme: MicroTheme) {}
+    @CallSuper
+    override fun onPause() {
+        this.lifecycleSubject.onNext(ActivityEvent.PAUSE)
+        super.onPause()
+    }
 
+    @CallSuper
+    override fun onStop() {
+        this.lifecycleSubject.onNext(ActivityEvent.STOP)
+        super.onStop()
+    }
+
+    @CallSuper
     override fun onDestroy() {
-        MicroThemeManager.unregister(this)
+        this.lifecycleSubject.onNext(ActivityEvent.DESTROY)
         super.onDestroy()
     }
 }
